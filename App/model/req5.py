517ca42fbcbd3bd15baef8bc7974e6dbac4b2dfd
TweepCoding <tweepcoding@gmail.com>
2021-11-10 21:29:32 -0500

    Req5 done for now

--- App/model/req5.py
+++ App/model/req5.py
@@ -19,7 +19,22 @@ def getLocationRange(catalog, ltmin, ltmax, lgmin, lgmax):
     latitude_values = om.values(latitude, ltmin, ltmax)
 
     for list in lt.iterator(latitude_values):
-        
+        filtered_list = lt.newList('ARRAY_LIST')
         for value in lt.iterator(list):
-            lt.addLast()
+            if value > lgmin and value < lgmax:
+                lt.addLast(filtered_list, value)
+        ms.sort(filtered_list, lambda value1, value2: float(value1['longitude']) > float(value2['longitude']))
+        for value in lt.iterator(filtered_list):
+            lt.addLast(values, value)
+    
+    samples = lt.subList(values, 1, 5)
+
+    last_samples = lt.subList(values, lt.size(values)-2, 5)
+
+    for value in lt.iterator(last_samples):
+        lt.addLast(samples, value)
+    
+    return samples ,lt.size(values)
+
+        
     
