c092ffa71b8b8f72d33102956471f5eec6379d7f
TweepCoding <tweepcoding@gmail.com>
2021-11-10 19:23:37 -0500

    Created requirement files, worked on req3

---
+++ App/model/req5.py
@@ -0,0 +1,8 @@
+import sys
+import config as cf
+from DISClib.ADT import list as lt
+from DISClib.ADT import map as mp
+from DISClib.DataStructures import mapentry as me
+from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.ADT import orderedmap as om
+import model.comp as cp
